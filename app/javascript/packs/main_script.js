b4126127e11c9028844a39d23820b47a232276a5
Hassan <hasalimah@gmail.com>
2022-01-27 18:15:08 -0500

    Load and filtered data into array of objects

--- app/javascript/packs/main_script.js
+++ app/javascript/packs/main_script.js
@@ -1,107 +1,119 @@
 import * as d3 from "d3"
 
 const parseTime = d3.timeParse("%Y-%m-%d");
-        var mapWidth = 900,
-            mapHeight = 600;
+var mapWidth = 900,
+  mapHeight = 600;
 
-        var c10 = d3.scaleOrdinal(d3.schemeCategory10);
+var c10 = d3.scaleOrdinal(d3.schemeCategory10);
 
-        var projection = d3.geoAlbers();
+var projection = d3.geoAlbers();
 
-        var path = d3.geoPath()
-           .projection(projection);
+var path = d3.geoPath()
+  .projection(projection);
 
-        var svg = d3.select(".wrapper").append("svg")
-            .attr("width", mapWidth)
-            .attr("height", mapHeight);
+var svg = d3.select(".wrapper").append("svg")
+  .attr("width", mapWidth)
+  .attr("height", mapHeight);
 
-        var mapLabel = svg.append("text")
-            .attr("y", 20)
-            .attr("x", 0)
-            .attr("class", "map_province_name")
+var mapLabel = svg.append("text")
+  .attr("y", 20)
+  .attr("x", 0)
+  .attr("class", "map_province_name")
 
-        // load TopoJSON file of Canada
-        d3.json("/canadaprovtopo.json", function(error, canada) {
-          if (error) throw error;
+// load TopoJSON file of Canada
+d3.json("/canadaprovtopo.json").then(function (canada) {
+  //if (error) throw error;
 
-          var provinces = topojson.feature(canada, canada.objects.canadaprov); 
+  var provinces = topojson.feature(canada, canada.objects.canadaprov);
 
-          // set default projection values 
-          projection
-              .scale(1)
-              .translate([0, 0]);
+  // set default projection values 
+  projection
+    .scale(1)
+    .translate([0, 0]);
 
-          // creates bounding box and helps with projection and scaling
-          var b = path.bounds(provinces),
-              s = .95 / Math.max((b[1][0] - b[0][0]) / mapWidth, (b[1][1] - b[0][1]) / mapHeight),
-              t = [(mapWidth - s * (b[1][0] + b[0][0])) / 2, (mapHeight - s * (b[1][1] + b[0][1])) / 2];
+  // creates bounding box and helps with projection and scaling
+  var b = path.bounds(provinces),
+    s = .95 / Math.max((b[1][0] - b[0][0]) / mapWidth, (b[1][1] - b[0][1]) / mapHeight),
+    t = [(mapWidth - s * (b[1][0] + b[0][0])) / 2, (mapHeight - s * (b[1][1] + b[0][1])) / 2];
 
-          // set project with bounding box data
-          projection
-              .scale(s)
-              .translate(t);
+  // set project with bounding box data
+  projection
+    .scale(s)
+    .translate(t);
 
-          // get individual provinces
-          svg.selectAll("path")
-                .data(provinces.features)
-              .enter().append("path")
-                .attr("class", "map_province")
-                .attr("d", path)
-                .on("mouseover", mouseover) 
-                .on("mouseout", mouseout)
-                .on("click", clicked)
+  // get individual provinces
+  svg.selectAll("path")
+    .data(provinces.features)
+    .enter().append("path")
+    .attr("class", "map_province")
+    .attr("d", path)
+    .on("mouseover", mouseover)
+    .on("mouseout", mouseout)
+    .on("click", clicked)
 
-          // add the mesh/path between provinces
-          svg.append("path")
-                .datum(topojson.mesh(canada, canada.objects.canadaprov, function(a, b) { return a !== b; }))
-                .attr("class", "map_mesh")
-                .attr("d", path);
-          
-          console.log(provinces.features);
+  // add the mesh/path between provinces
+  svg.append("path")
+    .datum(topojson.mesh(canada, canada.objects.canadaprov, function (a, b) { return a !== b; }))
+    .attr("class", "map_mesh")
+    .attr("d", path);
 
-        });
-        d3.csv("/prov_loc.csv", function(data) {
-            console.log(data);
-            svg.selectAll("circle")
-	        .data(data)
-	        .enter()
-	        .append("circle")
-	        .attr("cx", function(d) {
-		        return projection([d.long, d.lat])[0];
-                
-	        })
-	        .attr("cy", function(d) {
-		        return projection([d.long, d.lat])[1];
-	        })
-	        .attr("r", function(d) {
-		        return 20;
-	        })
-		    .style("fill", "rgb(217,91,67)")	
-		    .style("opacity", 0.85)	
-            
-        });
-        d3.csv('/vaccinations.csv', function(d){
-            
-            
-            return{
-                province: d.prename,
-                doseTotal: +d.numtotal_all_administered,
-                dose1: +d.numtotal_dose1_administered,
-                dose2: +d.numtotal_dose2_administered,
-                dose3: +d.numtotal_dose3_administered
-                //date: parseTime(d.report_date)
-            }
-            
-        });
-        function mouseover(d) {     
-          mapLabel.text(d.properties.name.slice(0,-5)) // remove suffix id from name
-        }
+  console.log(provinces.features);
+  console.log(provinces.features[0].properties.name)
 
-        function mouseout(d) {     
-          mapLabel.text("")  // remove out name
-        }
+});
+d3.csv("/prov_loc.csv").then(function (data) {
+  console.log(data);
+  svg.selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", function (d) {
+      return projection([d.long, d.lat])[0];
 
-        function clicked(d) {
-          console.log(d.id, d.properties.name) // verify everything looks good
-          // Add code here
-        }
+    })
+    .attr("cy", function (d) {
+      return projection([d.long, d.lat])[1];
+    })
+    .attr("r", function (d) {
+      return 20;
+    })
+    .style("fill", "rgb(217,91,67)")
+    .style("opacity", 0.85)
+
+});
+var freqData;
+//var filteredData;
+d3.csv('/vaccinations.csv').then( (data) => {
+    var filteredData = data.filter((row)=>{  
+      return row['report_date'].slice(8,10) == "28";
+  });
+  
+  console.log("filteredData",filteredData);
+  freqData = filteredData.map((d)=>{
+    return {
+      Province: d.prename,
+      DoseTotal: +d.numtotal_all_administered,
+      Dose1: +d.numtotal_dose1_administered,
+      Dose2: +d.numtotal_dose2_administered,
+      Dose3: +d.numtotal_dose3_administered,
+      Date: parseTime(d.report_date)
+    };
+  })
+  
+  console.log("freqData",freqData);
+
+}).then((data) => {
+  console.log("bigdata",data);
+});
+let mouseover = function (d) {
+
+  mapLabel.text(d.srcElement.__data__.properties.name) // remove suffix id from name
+}
+
+function mouseout(d) {
+  mapLabel.text("")  // remove out name
+}
+
+function clicked(d) {
+  //Much Later
+}
